df2756cb6a351ce71affb73ac54b31c63f3ac6b0
BuenoSabrina <sabrinabueno3@outlook.com>
2016-04-06 16:02:35 -0300

    Create soma.js

---
+++ Exercicio3/soma.js
@@ -0,0 +1,11 @@
+function soma(){
+	var var1 = document.querySelector("#var1").value;
+	var var2 = document.querySelector("#var2").value;
+	var resultado = parseInt(resultado);
+	var numero1 = parseInt(var1);
+	var numero2 = parseInt(var2);
+
+	resultado = numero1 + numero2;
+
+	document.querySelector("#resul").value = resultado;
+}
